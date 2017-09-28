c9bc25465b121c1223be2f3534921bd0a99a6689
Unknown <darkspark52@gmail.com>
2017-09-28 16:50:20 -0400

    Stuff

---
+++ RaycastPlatformer/Assets/PlayerController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float acceleration = 10;
+    public float jumpTime = .75f;
+    public float jumpHeight = 3;
+
+    float gravity;
+    float jumpImpulse;
+
+    Vector3 velocity = new Vector3();
+    PawnAABB pawn;
+
+    bool isGrounded = false;
+
+    void Start()
+    {
+        pawn = GetComponent<PawnAABB>();
+        DeriveJumpValues();
+    }
+
+    void DeriveJumpValues()
+    {
+        gravity = (jumpHeight * 2) / (jumpTime * jumpTime);
+        jumpImpulse = gravity * jumpTime;
+    }
+
+    void Update()
+    {
+        HandleInput();
+
+        // Do the move:
+        PawnAABB.CollisionResults results = pawn.Move(velocity * Time.deltaTime);
+        if (results.hitTop || results.hitBottom) velocity.y = 0;
+        if (results.hitLeft || results.hitRight) velocity.x = 0;
+
+        isGrounded = results.hitBottom;
+
+
+        transform.position += results.distance;
+
+    }
+
+    private void HandleInput()
+    {
+        //Gravity
+        velocity.y -= gravity * Time.deltaTime;
+
+        //jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = jumpImpulse;
+        }
+
+        //sidways movement
+        float axisH = Input.GetAxisRaw("Horizontal");
+
+        if (axisH == 0)
+        {
+            //slow down the player
+            Decelerate(acceleration);
+        }
+        else
+        {
+            AccelerateX(axisH * acceleration);
+        }
+            
+        
+    }
+
+    private void Decelerate(float amount)
+    {
+        if (velocity.x > 0) //moving right
+        {
+            AccelerateX(-amount);
+            if (velocity.x < 0) velocity.x = 0;
+        }
+        if (velocity.x < 0) //moving left
+        {
+            AccelerateX(amount);
+            if (velocity.x > 0) velocity.x = 0;
+        }
+    }
+
+    private void AccelerateX(float amount)
+    {
+        velocity.x += amount * Time.deltaTime;
+    }
+}
---
+++ Raycasting/Assets/CameraRaycaster.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRaycaster : MonoBehaviour {
+
+    public GameObject prefab;
+    public GameObject cylindar;
+    public LayerMask layerMask;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1")){
+            DoRaycast();
+        }
+        //DoRaycast();
+    }
+
+    private void DoRaycast()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        Debug.DrawRay(ray.origin, ray.direction, Color.yellow, 5);
+
+        if(Physics.Raycast(ray, out hit, 10, layerMask))
+        {
+            //hit.collider.GetComponent<MeshRenderer>().material.color = Color.red;
+
+            Quaternion rot = Quaternion.FromToRotation(Vector3.up, hit.normal);
+
+            //Instantiate(prefab, hit.point, rot);
+            cylindar.transform.position = hit.point;
+            cylindar.transform.rotation = rot;
+
+            print("succeess?");
+        }
+    }
+}
